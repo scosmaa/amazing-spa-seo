cbb0c96c532e38a21fc129ee05898ff965dbd91e
Simone Cosma <scosmaa@gmail.com>
2018-05-26 17:08:03 +0200

    Social share draft

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,15 @@
 const functions = require('firebase-functions');
 const express = require('express');
-
+const data = require('./data/data.json');
 const app = express();
 
 app.get('*', (req, res) => {
-    res.sendfile('public/index.html');
+    console.log(req.headers['user-agent']);
+    if (req.headers['user-agent'].toLocaleLowerCase().indexOf('twitter') > -1 || req.headers['user-agent'].toLocaleLowerCase().indexOf('facebook') > -1) {
+        res.sendfile('public/twitter.html');
+    } else {
+        res.sendfile('public/index.html');
+    }
 })
 
 exports.app = functions.https.onRequest(app);
